795a24198c129998aa42ff35bfe28da292bb62b3
Justin Boswell <boswej@amazon.com>
2020-04-21 16:34:57 -0700

    Fixed up build config, samples can be run now

--- android/app/src/main/java/software/amazon/awssdk/iotsamples/MainActivity.kt
+++ android/app/src/main/java/software/amazon/awssdk/iotsamples/MainActivity.kt
@@ -7,8 +7,11 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
 import java.io.OutputStream
 import java.io.PrintStream
+import java.lang.Exception
 import kotlin.concurrent.thread
 
 val SAMPLES = mapOf(
@@ -57,6 +60,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         setContentView(R.layout.activity_main)
 
         console = findViewById<TextView>(R.id.console)
+        console?.isEnabled = false
+
         sampleSelect = findViewById<Spinner>(R.id.sampleSelect);
 
         val samples = SAMPLES.values.toMutableList()
@@ -85,6 +90,22 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         }
     }
 
+    private fun assetContents(assetName: String) : String {
+        resources.assets.open(assetName).use {res ->
+            val bytes = ByteArray(res.available())
+            res.read(bytes)
+            return String(bytes).trim()
+        }
+    }
+
+    private fun assetContentsOr(assetName: String, defaultValue: String) : String {
+        return try {
+            assetContents(assetName)
+        } catch (fnf: FileNotFoundException) {
+            defaultValue
+        }
+    }
+
     private fun runSample(name: String) {
         val classLoader = Thread.currentThread().contextClassLoader
         val sampleClass = classLoader.loadClass(name);
@@ -92,10 +113,45 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             clearConsole()
             writeToConsole("Could not find sample '${name}'")
         }
-        val main = sampleClass.getMethod("main", Array<String>::class.java)
-        val args = arrayOf("")
+
         thread(name="sample_runner", contextClassLoader = classLoader) {
-            main.invoke(null, args);
+            // find resources, copy them into the cache so samples can get paths to them
+            val resourceNames = listOf("AmazonRootCA1.pem", "ca-certificates.crt", "certificate.pem", "privatekey.pem")
+            val resourceMap = HashMap<String, String>()
+            for (resourceName in resourceNames) {
+                resources.assets.open(resourceName).use { res ->
+                    val cachedName = "${externalCacheDir}/${resourceName}"
+                    FileOutputStream(cachedName).use { cachedRes ->
+                        res.copyTo(cachedRes)
+                    }
+
+                    resourceMap[resourceName] = cachedName
+                }
+            }
+
+            var endpoint: String = ""
+            resources.assets.open("endpoint.txt").use {res ->
+                val bytes = ByteArray(res.available())
+                res.read(bytes)
+                endpoint = String(bytes).trim()
+            }
+
+            val args = arrayOf(
+                "--endpoint", assetContents("endpoint.txt"),
+                "--rootca", resourceMap["AmazonRootCA1.pem"],
+                "--cert", resourceMap["certificate.pem"],
+                "--key", resourceMap["privatekey.pem"],
+                "--port", assetContentsOr("port.txt", "8883"),
+                "--clientId", assetContentsOr("clientId.txt", "android-java-crt-test"),
+                "--topic", assetContentsOr("topic.txt", "/samples/test"),
+                "--message", assetContentsOr("message.txt", "Hello World From Android")
+            )
+            val main = sampleClass.getMethod("main", Array<String>::class.java)
+            try {
+                main.invoke(null, args);
+            } catch (e: Exception) {
+                writeToConsole(e.toString())
+            }
             onSampleComplete();
         }
     }
