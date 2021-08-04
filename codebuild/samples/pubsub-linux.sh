baf1a9c694e763dbc643d445f7467c12baa3efaf
Bret Ambrose <bretambrose@gmail.com>
2021-08-04 11:16:02 -0700

    Debugging output

--- codebuild/samples/pubsub-linux.sh
+++ codebuild/samples/pubsub-linux.sh
@@ -6,6 +6,9 @@ env
 
 pushd $CODEBUILD_SRC_DIR/samples/BasicPubSub
 
+echo $ENDPOINT
+ls /tmp
+
 mvn compile
 mvn exec:java -Dexec.mainClass="pubsub.PubSub" -Daws.crt.ci="True" -Dexec.arguments="--endpoint,$ENDPOINT,--key,/tmp/privatekey.pem,--cert,/tmp/certificate.pem"
 
