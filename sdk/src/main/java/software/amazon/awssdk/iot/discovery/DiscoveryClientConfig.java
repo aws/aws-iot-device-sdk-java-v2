af25c1728c74101f08959bd1538577b1794fb96b
Joseph Klix <jkl@amazon.com>
2021-12-01 11:19:38 -0800

    add javadoc comments and remove region parameter

--- sdk/src/main/java/software/amazon/awssdk/iot/discovery/DiscoveryClientConfig.java
+++ sdk/src/main/java/software/amazon/awssdk/iot/discovery/DiscoveryClientConfig.java
@@ -15,6 +15,16 @@ public class DiscoveryClientConfig implements AutoCloseable {
     final private HttpProxyOptions proxyOptions;
     final private String ggServerName;
 
+    /**
+     * Constructor for DiscoveryClientConfig creates the correct endpoint if not in a special region
+     * 
+     * @param bootstrap ClientBootstrap
+     * @param tlsContextOptions TlsContextOptions
+     * @param socketOptions SocketOptions
+     * @param region AWS region. Not used when ggServerName is specified
+     * @param maxConnections integer of max connections
+     * @param proxyOptions HttpProxyOptions
+     */
     public DiscoveryClientConfig(
             final ClientBootstrap bootstrap,
             final TlsContextOptions tlsContextOptions,
@@ -31,18 +41,27 @@ public class DiscoveryClientConfig implements AutoCloseable {
         this.ggServerName = "";
     }
 
+    /**
+     * Default Constructor for DiscoveryClientConfig that allows the specification of a specific ggServerName to use in special regions
+     * 
+     * @param bootstrap ClientBootstrap
+     * @param tlsContextOptions TlsContextOptions
+     * @param socketOptions SocketOptions
+     * @param maxConnections integer of max connections
+     * @param proxyOptions HttpProxyOptions
+     * @param ggServerName full endpoint to use when connecting in special regions
+     */
     public DiscoveryClientConfig(
             final ClientBootstrap bootstrap,
             final TlsContextOptions tlsContextOptions,
             final SocketOptions socketOptions,
-            final String region,
             final int maxConnections,
             final HttpProxyOptions proxyOptions,
             final String ggServerName) {
         this.bootstrap = bootstrap;
         this.tlsContext = new TlsContext(tlsContextOptions);
         this.socketOptions = socketOptions;
-        this.region = region;
+        this.region = "";
         this.maxConnections = maxConnections;
         this.proxyOptions = proxyOptions;
         this.ggServerName = ggServerName;
