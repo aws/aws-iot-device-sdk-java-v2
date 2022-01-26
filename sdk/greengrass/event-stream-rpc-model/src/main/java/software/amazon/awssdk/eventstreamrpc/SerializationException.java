69796bf1636a61bf2ec342e2438cc0cbdb9d1409
Michael Dombrowski <mdombro@amazon.com>
2022-01-26 13:59:00 -0500

    add prints

--- sdk/greengrass/event-stream-rpc-model/src/main/java/software/amazon/awssdk/eventstreamrpc/SerializationException.java
+++ sdk/greengrass/event-stream-rpc-model/src/main/java/software/amazon/awssdk/eventstreamrpc/SerializationException.java
@@ -6,6 +6,6 @@ public class SerializationException extends RuntimeException {
     }
 
     public SerializationException(Object object, Throwable cause) {
-        super("Could not serialize object: " + object.toString());
+        super("Could not serialize object: " + object.toString(), cause);
     }
 }
