0e6342e2503745c853e69813445425b69269b3e2
Noah Beard <ncbeard@amazon.com>
2022-03-21 18:04:54 -0400

    Added builder creation functions to command line utils and simplified pubsub sample

--- samples/BasicPubSub/src/main/java/pubsub/PubSub.java
+++ samples/BasicPubSub/src/main/java/pubsub/PubSub.java
@@ -81,72 +81,19 @@ public class PubSub {
         cmdUtils = new CommandLineUtils();
         cmdUtils.registerProgramName("PubSub");
         cmdUtils.addCommonMQTTCommands();
+        cmdUtils.addCommonTopicMessageCommands();
         cmdUtils.registerCommand("key", "<path>", "Path to your key in PEM format.");
         cmdUtils.registerCommand("cert", "<path>", "Path to your client certificate in PEM format.");
         cmdUtils.registerCommand("client_id", "<int>", "Client id to use (optional, default='test-*').");
         cmdUtils.registerCommand("port", "<int>", "Port to connect to on the endpoint (optional, default='8883').");
-        cmdUtils.registerCommand("topic", "<str>", "Topic to subscribe/publish to (optional, default='test/topic').");
-        cmdUtils.registerCommand("message", "<str>", "Message to publish (optional, default='Hello World').");
         cmdUtils.registerCommand("count", "<int>", "Number of messages to publish (optional, default='10').");
-        cmdUtils.registerCommand("use_websocket", "", "Use websockets (optional).");
-        cmdUtils.registerCommand("x509", "", "Use the x509 credentials provider while using websockets (optional).");
-        cmdUtils.registerCommand("x509_role_alias", "<str>", "Role alias to use with the x509 credentials provider (required for x509).");
-        cmdUtils.registerCommand("x509_endpoint", "<str>", "Endpoint to fetch x509 credentials from (required for x509).");
-        cmdUtils.registerCommand("x509_thing", "<str>", "Thing name to fetch x509 credentials on behalf of (required for x509).");
-        cmdUtils.registerCommand("x509_cert", "<path>", "Path to the IoT thing certificate used in fetching x509 credentials (required for x509).");
-        cmdUtils.registerCommand("x509_key", "<path>", "Path to the IoT thing private key used in fetching x509 credentials (required for x509).");
-        cmdUtils.registerCommand("x509_ca_file", "<path>", "Path to the root certificate used in fetching x509 credentials (required for x509).");
-        cmdUtils.registerCommand("proxy_host", "<str>", "Websocket proxy host to use (optional, required if --proxy_port is set).");
-        cmdUtils.registerCommand("proxy_port", "<int>", "Websocket proxy port to use (optional, required if --proxy_host is set).");
-        cmdUtils.registerCommand("region", "<str>", "AWS IoT service region (optional, default='us-east-1').");
-
-        cmdUtils.registerCommand("help", "", "Prints this message");
         cmdUtils.sendArguments(args);
 
-        if (cmdUtils.hasCommand("help")) {
-            cmdUtils.printHelp();
-            System.exit(1);
-        }
-
-        endpoint = cmdUtils.getCommandRequired("endpoint", "");
-        clientId = cmdUtils.getCommandOrDefault("client_id", clientId);
-        port = Integer.parseInt(cmdUtils.getCommandOrDefault("port", String.valueOf(port)));
-        rootCaPath = cmdUtils.getCommandOrDefault("root_ca", rootCaPath);
         certPath = cmdUtils.getCommandOrDefault("cert", certPath);
         keyPath = cmdUtils.getCommandOrDefault("key", keyPath);
         topic = cmdUtils.getCommandOrDefault("topic", topic);
         message = cmdUtils.getCommandOrDefault("message", message);
         messagesToPublish = Integer.parseInt(cmdUtils.getCommandOrDefault("count", String.valueOf(messagesToPublish)));
-        useWebsockets = cmdUtils.hasCommand("use_websocket");
-        useX509Credentials = cmdUtils.hasCommand("x509");
-        if (useX509Credentials) {
-            useWebsockets = true;
-        }
-        x509RoleAlias = cmdUtils.getCommandOrDefault("x509_role_alias", x509RoleAlias);
-        x509Endpoint = cmdUtils.getCommandOrDefault("x509_endpoint", x509Endpoint);
-        x509Thing = cmdUtils.getCommandOrDefault("x509_thing", x509Thing);
-        x509CertPath = cmdUtils.getCommandOrDefault("x509_cert", x509CertPath);
-        x509KeyPath = cmdUtils.getCommandOrDefault("x509_key", x509KeyPath);
-        x509RootCaPath = cmdUtils.getCommandOrDefault("x509_ca_file", x509RootCaPath);
-        proxyHost = cmdUtils.getCommandOrDefault("proxy_host", proxyHost);
-        proxyPort = Integer.parseInt(cmdUtils.getCommandOrDefault("proxy_port", String.valueOf(proxyPort)));
-        region = cmdUtils.getCommandOrDefault("region", region);
-
-        if (useWebsockets == false) {
-            if (certPath == null || keyPath == null) {
-                cmdUtils.printHelp();
-                System.out.println("--cert and --key required if not using --use_websocket.");
-                onApplicationFailure(null);
-                return;
-            }
-        } else if (useX509Credentials) {
-            if (x509RoleAlias == null || x509Endpoint == null || x509Thing == null || x509CertPath == null || x509KeyPath == null) {
-                cmdUtils.printHelp();
-                System.out.println("--x509_role_alias, --x509_endpoint, --x509_thing, --x509_cert, and --x509_key required if using x509.");
-                onApplicationFailure(null);
-                return;
-            }
-        }
 
         MqttClientConnectionEvents callbacks = new MqttClientConnectionEvents() {
             @Override
@@ -162,92 +109,48 @@ public class PubSub {
             }
         };
 
-        try (
-            AwsIotMqttConnectionBuilder builder = AwsIotMqttConnectionBuilder.newMtlsBuilderFromPath(certPath, keyPath)) {
+        try {
 
-            if (rootCaPath != null) {
-                builder.withCertificateAuthorityFromPath(null, rootCaPath);
+            MqttClientConnection connection = cmdUtils.buildMQTTConnection(callbacks);
+            if (connection == null)
+            {
+                onApplicationFailure(new RuntimeException("MQTT connection creation failed!"));
             }
 
-            builder.withConnectionEventCallbacks(callbacks)
-                    .withClientId(clientId)
-                    .withEndpoint(endpoint)
-                    .withPort((short)port)
-                    .withCleanSession(true)
-                    .withProtocolOperationTimeoutMs(60000);
-
-            HttpProxyOptions proxyOptions = null;
-            if (proxyHost != null && proxyPort > 0) {
-                proxyOptions = new HttpProxyOptions();
-                proxyOptions.setHost(proxyHost);
-                proxyOptions.setPort(proxyPort);
-
-                builder.withHttpProxyOptions(proxyOptions);
+            CompletableFuture<Boolean> connected = connection.connect();
+            try {
+                boolean sessionPresent = connected.get();
+                System.out.println("Connected to " + (!sessionPresent ? "new" : "existing") + " session!");
+            } catch (Exception ex) {
+                throw new RuntimeException("Exception occurred during connect", ex);
             }
 
-            if (useWebsockets) {
-                builder.withWebsockets(true);
-                builder.withWebsocketSigningRegion(region);
+            CountDownLatch countDownLatch = new CountDownLatch(messagesToPublish);
 
-                if (useX509Credentials) {
-                    try (TlsContextOptions x509TlsOptions = TlsContextOptions.createWithMtlsFromPath(x509CertPath, x509KeyPath)) {
-                        if (x509RootCaPath != null) {
-                            x509TlsOptions.withCertificateAuthorityFromPath(null, x509RootCaPath);
-                        }
+            CompletableFuture<Integer> subscribed = connection.subscribe(topic, QualityOfService.AT_LEAST_ONCE, (message) -> {
+                String payload = new String(message.getPayload(), StandardCharsets.UTF_8);
+                System.out.println("MESSAGE: " + payload);
+                countDownLatch.countDown();
+            });
 
-                        try (ClientTlsContext x509TlsContext = new ClientTlsContext(x509TlsOptions)) {
-                            X509CredentialsProvider.X509CredentialsProviderBuilder x509builder = new X509CredentialsProvider.X509CredentialsProviderBuilder()
-                                    .withTlsContext(x509TlsContext)
-                                    .withEndpoint(x509Endpoint)
-                                    .withRoleAlias(x509RoleAlias)
-                                    .withThingName(x509Thing)
-                                    .withProxyOptions(proxyOptions);
-                            try (X509CredentialsProvider provider = x509builder.build()) {
-                                builder.withWebsocketCredentialsProvider(provider);
-                            }
-                        }
-                    }
-                }
+            subscribed.get();
+
+            int count = 0;
+            while (count++ < messagesToPublish) {
+                CompletableFuture<Integer> published = connection.publish(new MqttMessage(topic, message.getBytes(), QualityOfService.AT_LEAST_ONCE, false));
+                published.get();
+                Thread.sleep(1000);
             }
 
-            try(MqttClientConnection connection = builder.build()) {
+            countDownLatch.await();
 
-                CompletableFuture<Boolean> connected = connection.connect();
-                try {
-                    boolean sessionPresent = connected.get();
-                    System.out.println("Connected to " + (!sessionPresent ? "new" : "existing") + " session!");
-                } catch (Exception ex) {
-                    throw new RuntimeException("Exception occurred during connect", ex);
-                }
-
-                CountDownLatch countDownLatch = new CountDownLatch(messagesToPublish);
-
-                CompletableFuture<Integer> subscribed = connection.subscribe(topic, QualityOfService.AT_LEAST_ONCE, (message) -> {
-                    String payload = new String(message.getPayload(), StandardCharsets.UTF_8);
-                    System.out.println("MESSAGE: " + payload);
-                    countDownLatch.countDown();
-                });
-
-                subscribed.get();
-
-                int count = 0;
-                while (count++ < messagesToPublish) {
-                    CompletableFuture<Integer> published = connection.publish(new MqttMessage(topic, message.getBytes(), QualityOfService.AT_LEAST_ONCE, false));
-                    published.get();
-                    Thread.sleep(1000);
-                }
-
-                countDownLatch.await();
-
-                CompletableFuture<Void> disconnected = connection.disconnect();
-                disconnected.get();
-            }
+            CompletableFuture<Void> disconnected = connection.disconnect();
+            disconnected.get();
         } catch (CrtRuntimeException | InterruptedException | ExecutionException ex) {
             onApplicationFailure(ex);
         }
 
         CrtResource.waitForNoResources();
-
         System.out.println("Complete!");
     }
 }
