ac2cddd5a2d07b963889e3dcc27d874393dfc5bf
Michael Graeb <graebm@amazon.com>
2022-04-21 21:04:43 -0700

    experiment 4

--- make-docs.py
+++ make-docs.py
@@ -2,13 +2,7 @@
 import os
 import shutil
 
-if os.system('mvn clean') != 0:
-    exit(1)
-
-if os.system('mvn install') != 0:
-    exit(1)
-
-if os.system('mvn javadoc:javadoc') != 0:
+if os.system('mvn clean javadoc:javadoc') != 0:
     exit(1)
 
 if os.path.exists('docs'):
