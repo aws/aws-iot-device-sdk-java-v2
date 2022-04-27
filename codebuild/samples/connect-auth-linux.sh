0863fa4f01545c8d6a8e8c559ba4fdf951f922ed
Noah Beard <ncbeard@amazon.com>
2022-04-27 16:19:31 -0400

    Remove passing CA file now that it is no longer saved in CI

--- codebuild/samples/connect-auth-linux.sh
+++ codebuild/samples/connect-auth-linux.sh
@@ -11,6 +11,6 @@ ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" 
 mvn compile
 
 echo "Mqtt Connect with Custom Authorizer test"
-mvn exec:java -Dexec.mainClass="customauthorizerconnect.CustomAuthorizerConnect" -Daws.crt.ci="True" -Daws.crt.debugnative="True" -Daws.crt.log.level="Trace" -Daws.crt.log.destination="Stdout" -Dexec.arguments="--endpoint,$ENDPOINT,--cert,/tmp/certificate.pem,--key,/tmp/privatekey.pem,--ca_file,/tmp/rootca.pem,--auth_name,TestSDKAuthorizer,--auth_username,V2SDK"
+mvn exec:java -Dexec.mainClass="customauthorizerconnect.CustomAuthorizerConnect" -Daws.crt.ci="True" -Daws.crt.debugnative="True" -Daws.crt.log.level="Trace" -Daws.crt.log.destination="Stdout" -Dexec.arguments="--endpoint,$ENDPOINT,--cert,/tmp/certificate.pem,--key,/tmp/privatekey.pem,--auth_name,TestSDKAuthorizer,--auth_username,V2SDK"
 
 popd
