54a36725bcdc1494c42d63c94e56b9ccea37a217
Noah Beard <ncbeard@amazon.com>
2022-04-27 16:09:48 -0400

    Fix Codebuild log printing

--- codebuild/samples/connect-auth-linux.sh
+++ codebuild/samples/connect-auth-linux.sh
@@ -11,6 +11,6 @@ ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" 
 mvn compile
 
 echo "Mqtt Connect with Custom Authorizer test"
-mvn exec:java -Dexec.mainClass="customauthorizerconnect.CustomAuthorizerConnect" -Daws.crt.ci="True" -Dexec.arguments="--endpoint,$ENDPOINT,--cert,/tmp/certificate.pem,--key,/tmp/privatekey.pem,--ca_file,/tmp/rootca.pem,--auth_name,TestSDKAuthorizer,--auth_username,V2SDK" -Daws.crt.debugnative=true -Daws.crt.log.level=Trace, -Daws.crt.log.destination=Stdout
+mvn exec:java -Dexec.mainClass="customauthorizerconnect.CustomAuthorizerConnect" -Daws.crt.ci="True" -Daws.crt.debugnative="True" -Daws.crt.log.level="Trace" -Daws.crt.log.destination="Stdout" -Dexec.arguments="--endpoint,$ENDPOINT,--cert,/tmp/certificate.pem,--key,/tmp/privatekey.pem,--ca_file,/tmp/rootca.pem,--auth_name,TestSDKAuthorizer,--auth_username,V2SDK"
 
 popd
