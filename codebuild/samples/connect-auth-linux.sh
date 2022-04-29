42e15ad016bf2e1281e620d890e691fb117a75bb
Noah Beard <ncbeard@amazon.com>
2022-04-29 14:22:33 -0400

    Remove authorizer name and username, use secrets

--- codebuild/samples/connect-auth-linux.sh
+++ codebuild/samples/connect-auth-linux.sh
@@ -8,9 +8,12 @@ pushd $CODEBUILD_SRC_DIR/samples/CustomAuthorizerConnect
 
 ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
 
+AUTH_NAME=$(aws secretsmanager get-secret-value --secret-id "unit-test/authorizer-name" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
+AUTH_PASSWORD=$(aws secretsmanager get-secret-value --secret-id "unit-test/authorizer-password" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
+
 mvn compile
 
 echo "Mqtt Connect with Custom Authorizer test"
-mvn exec:java -Dexec.mainClass="customauthorizerconnect.CustomAuthorizerConnect" -Daws.crt.ci="True" -Dexec.arguments="--endpoint,$ENDPOINT,--auth_name,TestSDKAuthorizer,--auth_username,V2SDK"
+mvn exec:java -Dexec.mainClass="customauthorizerconnect.CustomAuthorizerConnect" -Daws.crt.ci="True" -Dexec.arguments="--endpoint,$ENDPOINT,--auth_name,$AUTH_NAME,--auth_password,$AUTH_PASSWORD"
 
 popd
