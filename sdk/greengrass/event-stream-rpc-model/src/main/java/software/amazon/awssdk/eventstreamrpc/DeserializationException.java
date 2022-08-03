8727837c26d2c53d1b3ac43f82fe2c0bed4a748d
Tim Mattison <tim@mattison.org>
2022-08-03 17:05:09 -0400

    Added code to print out byte array data

--- sdk/greengrass/event-stream-rpc-model/src/main/java/software/amazon/awssdk/eventstreamrpc/DeserializationException.java
+++ sdk/greengrass/event-stream-rpc-model/src/main/java/software/amazon/awssdk/eventstreamrpc/DeserializationException.java
@@ -1,11 +1,21 @@
 package software.amazon.awssdk.eventstreamrpc;
 
+import java.util.Arrays;
+
 public class DeserializationException extends RuntimeException {
     public DeserializationException(Object lexicalData) {
         this(lexicalData, null);
     }
 
     public DeserializationException(Object lexicalData, Throwable cause) {
-        super("Could not deserialize data: [" + lexicalData.toString() + "]", cause);
+        super("Could not deserialize data: [" + stringify(lexicalData) + "]", cause);
+    }
+
+    private static String stringify(Object lexicalData) {
+        if (lexicalData instanceof byte[]) {
+            return Arrays.toString((byte[]) lexicalData);
+        }
+
+        return lexicalData.toString();
     }
 }
