931d162394337f076d62feaecd2ab136a7f08384
Noah Beard <ncbeard@amazon.com>
2022-09-20 15:55:49 -0400

    Add the new Codebuild files

---
+++ codebuild/new_samples/setup-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+env
+
+# build java package
+cd $CODEBUILD_SRC_DIR
+
+ulimit -c unlimited
+mvn compile
+mvn install -DskipTests=true
+
+cert=$(aws secretsmanager get-secret-value --secret-id "ci/PubSub/cert" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$cert" > /tmp/certificate.pem
+key=$(aws secretsmanager get-secret-value --secret-id "ci/PubSub/key" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$key" > /tmp/privatekey.pem
+key_p8=$(aws secretsmanager get-secret-value --secret-id "ci/PubSub/keyp8" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$key_p8" > /tmp/privatekey_p8.pem
+
