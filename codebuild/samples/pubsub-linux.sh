a3376ad27086ad87f63018a6ac5f309bc1e7bb4f
Noah Beard <ncbeard@amazon.com>
2022-10-21 11:29:38 -0400

    Add back all of the Codebuild samples, add shadows and PKCS11 to Codebuild

---
+++ codebuild/samples/pubsub-linux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+env
+
+pushd $CODEBUILD_SRC_DIR/samples/BasicPubSub
+
+ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "ci/endpoint" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
+
+mvn compile
+
+echo "Basic PubSub test"
+mvn exec:java -Dexec.mainClass="pubsub.PubSub" -Daws.crt.ci="True" -Dexec.arguments="--endpoint,$ENDPOINT,--key,/tmp/privatekey.pem,--cert,/tmp/certificate.pem"
+
+popd
