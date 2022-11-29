eb60888ba0f8ec6e59f92067ac8e7c40779f3bd6
Noah Beard <ncbeard@amazon.com>
2022-11-29 13:10:34 -0500

    Nevermind, the builder is just not possible currently without a decent amount of rework

--- utils/mqtt5_test_setup.sh
+++ utils/mqtt5_test_setup.sh
@@ -44,12 +44,8 @@ else
     exit 0
 fi
 
-# Is this a normal request or a readonly one? If normal, download the file, otherwise just assume it is there.
-if [ "${region}" != "readonly" ]; then
-    # Get the file from S3
-    aws s3 cp ${testing_env_bucket} ${PWD}/environment_files.txt
-fi
-
+# Get the file from S3
+aws s3 cp ${testing_env_bucket} ${PWD}/environment_files.txt
 testing_env_file=$( cat environment_files.txt )
 # Make sure we have data of some form
 if [ "${testing_env_file}" != "" ]; then
