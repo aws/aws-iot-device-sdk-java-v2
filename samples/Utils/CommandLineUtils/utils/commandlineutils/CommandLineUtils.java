8a25f459b363f1f3296c034cde19f68d5739ed28
Steve Kim <sbstevek@amazon.com>
2023-06-12 14:29:39 -0700

    add count parsing

--- samples/Utils/CommandLineUtils/utils/commandlineutils/CommandLineUtils.java
+++ samples/Utils/CommandLineUtils/utils/commandlineutils/CommandLineUtils.java
@@ -241,6 +241,7 @@ public class CommandLineUtils {
             returnData.input_topic = getCommandOrDefault(m_cmd_topic, "test/topic");
             returnData.input_message = getCommandOrDefault(m_cmd_message, "Hello World!");
         }
+        returnData.input_count = Integer.parseInt(getCommandOrDefault(m_cmd_count, "10"));
     }
 
     private void parseCommonProxyCommands(SampleCommandLineData returnData) {
