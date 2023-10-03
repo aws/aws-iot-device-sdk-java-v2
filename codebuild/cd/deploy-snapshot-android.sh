f643286719df4e1b7c6a7690be7432c769355691
Steve Kim <sbstevek@amazon.com>
2023-10-03 13:56:43 -0700

    store android repository id in secrets

--- codebuild/cd/deploy-snapshot-android.sh
+++ codebuild/cd/deploy-snapshot-android.sh
@@ -11,3 +11,5 @@ GPG_KEY=$(cat /tmp/aws-sdk-common-runtime.key.asc)
 ./gradlew -PsigningKey=$"$GPG_KEY" -PsigningPassword=$GPG_PASSPHRASE -PsonatypeUsername='aws-sdk-common-runtime' -PsonatypePassword=$ST_PASSWORD publishToAwsNexus closeAwsNexusStagingRepository | tee /tmp/android_deploy.log
 # Get the staging repository id and save it
 cat /tmp/android_deploy.log | grep "Created staging repository" | cut -d\' -f2 | tee /tmp/android_repositoryId.txt
+# Store the repository ID in a secret for use later
+aws secretsmanager update-secret --secret-id cd/aws-iot-device-sdk-java-v2/repository-id-android --secret-string "$(cat /tmp/repositoryId.txt)" --region us-east-1
