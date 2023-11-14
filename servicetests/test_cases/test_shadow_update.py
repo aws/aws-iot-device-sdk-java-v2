a4c06cdaebc6fe50526d6d8bdb3e6bc86f7d0a47
Igor Abdrakhimov <igorabd@amazon.com>
2023-11-14 15:41:58 -0800

    Add run script

---
+++ servicetests/test_cases/test_shadow_update.py
@@ -0,0 +1,59 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+
+import argparse
+import uuid
+import os
+import sys
+import run_in_ci
+import ci_iot_thing
+
+
+def main():
+    argument_parser = argparse.ArgumentParser(
+        description="Run Fleet Provisioning test in CI")
+    argument_parser.add_argument(
+        "--input-uuid", required=False, help="UUID for thing name. UUID will be generated if this option is omit")
+    argument_parser.add_argument(
+        "--region", required=False, default="us-east-1", help="The name of the region to use")
+    argument_parser.add_argument(
+        "--mqtt-version", required=True, choices=[3, 5], type=int, help="MQTT protocol version to use")
+    parsed_commands = argument_parser.parse_args()
+
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    cfg_file_pfx = "mqtt3_" if parsed_commands.mqtt_version == 3 else "mqtt5_"
+    cfg_file = os.path.join(current_path, cfg_file_pfx + "shadow_cfg.json")
+    input_uuid = parsed_commands.input_uuid if parsed_commands.input_uuid else str(uuid.uuid4())
+
+    thing_name = "ServiceTest_Shadow_" + input_uuid
+
+    policy_name = "CI_Shadow_Thing_Policy"
+
+    # Temporary certificate/key file path.
+    certificate_path = os.path.join(os.getcwd(), 'certificate.pem.crt')
+    key_path = os.path.join(os.getcwd(), 'private.pem.key')
+
+    ci_iot_thing.create_iot_thing(
+        thing_name=thing_name,
+        region=parsed_commands.region,
+        policy_name=policy_name,
+        certificate_path=certificate_path,
+        key_path=key_path)
+
+    sys.exit(0);
+
+    # Perform fleet provisioning. If it's successful, a newly created thing should appear.
+    test_result = run_in_ci.setup_and_launch(cfg_file, input_uuid)
+
+    # Delete a thing created by fleet provisioning.
+    # NOTE We want to try to delete thing even if test was unsuccessful.
+    thing_name = parsed_commands.thing_name_prefix + input_uuid
+    delete_result = ci_iot_thing.delete_iot_thing(
+        thing_name, parsed_commands.region)
+
+    if test_result != 0 or delete_result != 0:
+        sys.exit(-1)
+
+
+if __name__ == "__main__":
+    main()
--- utils/ci_iot_thing.py
+++ utils/ci_iot_thing.py
@@ -1,19 +1,121 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0.
 
+import sys
+
 import boto3
 
 
-def delete_iot_thing(thing_name, region):
+def create_iot_thing(thing_name, region, policy_name, certificate_path, key_path, thing_group=None):
+    """ Create IoT thing along with policy and credentials. """
+
     try:
         iot_client = boto3.client('iot', region_name=region)
     except Exception as e:
-        print(f"ERROR: Could not make Boto3 client. Credentials likely could not be sourced. Exception: {e}")
+        print(f"ERROR: Could not make Boto3 client. Credentials likely could not be sourced. Exception: {e}",
+              file=sys.stderr)
         return -1
 
+    # Create a thing.
+    try:
+        print("Creating thing '{thing_name}'", file=sys.stderr)
+        # create_thing_response:
+        # {
+        # 'thingName': 'string',
+        # 'thingArn': 'string',
+        # 'thingId': 'string'
+        # }
+        create_thing_response = iot_client.create_thing(
+            thingName=thing_name
+        )
+        if thing_group:
+            iot_client.add_thing_to_thing_group(
+                thingGroupName=thing_group,
+                thingName=thing_name,
+            )
+    except Exception as e:
+        print(f"ERROR: Failed to create thing '{thing_name}'; exception: {e}", file=sys.stderr)
+        return -1
+
+    # Create certificate and keys.
+    try:
+        print("Creating certificate", file=sys.stderr)
+        # create_cert_response:
+        # {
+        # 'certificateArn': 'string',
+        # 'certificateId': 'string',
+        # 'certificatePem': 'string',
+        # 'keyPair':
+        #   {
+        #     'PublicKey': 'string',
+        #     'PrivateKey': 'string'
+        #   }
+        # }
+        create_cert_response = iot_client.create_keys_and_certificate(
+            setAsActive=True
+        )
+        # Write certificate to file.
+        f = open(certificate_path, "w")
+        f.write(create_cert_response['certificatePem'])
+        f.close()
+
+        # Write private key to file.
+        f = open(key_path, "w")
+        f.write(create_cert_response['keyPair']['PrivateKey'])
+        f.close()
+    except Exception:
+        try:
+            iot_client.delete_thing(thingName=thing_name)
+        except Exception:
+            print("ERROR: Could not delete thing", file=sys.stderr)
+        print("ERROR: Failed to create certificate", file=sys.stderr)
+        return -1
+
+    certificate_arn = create_cert_response['certificateArn']
+    certificate_id = create_cert_response['certificateId']
+
+    # Attach policy to certificate.
+    try:
+        iot_client.attach_policy(
+            policyName=policy_name,
+            target=certificate_arn
+        )
+    except Exception as ex:
+        print(ex, file=sys.stderr)
+        delete_iot_thing(thing_name, region)
+        print("ERROR: Failed to attach policy", file=sys.stderr)
+        return -1
+
+    # Attach certificate to thing.
+    try:
+        print("Attaching certificate to thing", file=sys.stderr)
+        iot_client.attach_thing_principal(
+            thingName=thing_name,
+            principal=certificate_arn
+        )
+    except Exception:
+        delete_iot_thing(thing_name, region)
+        print("ERROR: Failed to attach certificate", file=sys.stderr)
+        return -1
+
+    print("IoT thing created successfully", file=sys.stderr)
+    return 0
+
+
+def delete_iot_thing(thing_name, region):
+    """ Delete IoT thing and all its principals. """
+
+    try:
+        iot_client = boto3.client('iot', region_name=region)
+    except Exception as e:
+        print(f"ERROR: Could not make Boto3 client. Credentials likely could not be sourced. Exception: {e}",
+              file=sys.stderr)
+        return -1
+
+    # Detach and delete thing's principals.
     try:
         thing_principals = iot_client.list_thing_principals(thingName=thing_name)
-        print(f"principals: {thing_principals}")
+        print(f"Detaching and deleting principals: {thing_principals}", file=sys.stderr)
         for principal in thing_principals["principals"]:
             certificate_id = principal.split("/")[1]
             iot_client.detach_thing_principal(thingName=thing_name, principal=principal)
@@ -21,14 +123,15 @@ def delete_iot_thing(thing_name, region):
             iot_client.delete_certificate(certificateId=certificate_id, forceDelete=True)
     except Exception as e:
         print("ERROR: Could not delete certificate for IoT thing "
-              f"{thing_name}, probably thing does not exist. Exception: {e}")
+              f"{thing_name}, probably thing does not exist. Exception: {e}", file=sys.stderr)
         return -1
 
+    # Delete thing.
     try:
         iot_client.delete_thing(thingName=thing_name)
     except Exception as e:
-        print(f"ERROR: Could not delete IoT thing {thing_name}. Exception: {e}")
+        print(f"ERROR: Could not delete IoT thing {thing_name}. Exception: {e}", file=sys.stderr)
         return -1
 
-    print("IoT thing deleted successfully")
+    print("IoT thing deleted successfully", file=sys.stderr)
     return 0
