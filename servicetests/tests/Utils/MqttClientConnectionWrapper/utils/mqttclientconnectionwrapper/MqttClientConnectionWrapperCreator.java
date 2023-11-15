b222b3f0482e5177bb59b81c63aeb5ccb30aec3a
Igor Abdrakhimov <igorabd@amazon.com>
2023-11-15 15:36:56 -0800

    CLen code

--- servicetests/tests/Utils/MqttClientConnectionWrapper/utils/mqttclientconnectionwrapper/MqttClientConnectionWrapperCreator.java
+++ servicetests/tests/Utils/MqttClientConnectionWrapper/utils/mqttclientconnectionwrapper/MqttClientConnectionWrapperCreator.java
@@ -13,46 +13,55 @@ import software.amazon.awssdk.iot.AwsIotMqtt5ClientBuilder;
 import ServiceTestLifecycleEvents.ServiceTestLifecycleEvents;
 
 final public class MqttClientConnectionWrapperCreator {
-    public static MqttClientConnectionWrapper createConnection(String cert, String key, String clientId, String endpoint,
-            int port, Integer mqttVersion) {
+    public static MqttClientConnectionWrapper createConnection(
+            String cert, String key, String clientId, String endpoint, int port, Integer mqttVersion) {
         if (mqttVersion == 3) {
-            try (AwsIotMqttConnectionBuilder builder = AwsIotMqttConnectionBuilder
-                    .newMtlsBuilderFromPath(cert, key)) {
-                builder.withClientId(clientId)
-                    .withEndpoint(endpoint)
-                    .withPort((short)port)
-                    .withCleanSession(true);
-                Mqtt3ClientConnectionWrapper connWrapper = new Mqtt3ClientConnectionWrapper();
-                connWrapper.connection = builder.build();
-                if (connWrapper.connection == null) {
-                    throw new RuntimeException("MQTT311 connection creation failed!");
-                }
-                return connWrapper;
-            } catch (Exception ex) {
-                throw new RuntimeException("Failed to create MQTT311 connection", ex);
-            }
+            return createMqtt3Connection(cert, key, clientId, endpoint, port);
         } else if (mqttVersion == 5) {
-            ServiceTestLifecycleEvents lifecycleEvents = new ServiceTestLifecycleEvents();
-            try (AwsIotMqtt5ClientBuilder builder = AwsIotMqtt5ClientBuilder.newDirectMqttBuilderWithMtlsFromPath(
-                        endpoint, cert, key)) {
-                ConnectPacket.ConnectPacketBuilder connectProperties = new ConnectPacket.ConnectPacketBuilder();
-                connectProperties.withClientId(clientId);
-                builder.withConnectProperties(connectProperties);
-                builder.withLifeCycleEvents(lifecycleEvents);
-                builder.withPort((long)port);
-                Mqtt5ClientConnectionWrapper connWrapper = new Mqtt5ClientConnectionWrapper();
-                connWrapper.client = builder.build();
-                connWrapper.connection = new MqttClientConnection(connWrapper.client, null);
-                if (connWrapper.connection == null) {
-                    throw new RuntimeException("MQTT5 connection creation failed!");
-                }
-                return connWrapper;
-            } catch (Exception ex) {
-                throw new RuntimeException("Failed to create MQTT311 connection from MQTT5 client", ex);
-            }
+            return createMqtt5Connection(cert, key, clientId, endpoint, port);
         } else {
             throw new RuntimeException("Invalid MQTT version specified: " + mqttVersion);
         }
     }
 
+    static MqttClientConnectionWrapper createMqtt3Connection(
+            String cert, String key, String clientId, String endpoint, int port) {
+        try (AwsIotMqttConnectionBuilder builder = AwsIotMqttConnectionBuilder
+                .newMtlsBuilderFromPath(cert, key)) {
+            builder.withClientId(clientId)
+                .withEndpoint(endpoint)
+                .withPort((short)port)
+                .withCleanSession(true);
+            Mqtt3ClientConnectionWrapper connWrapper = new Mqtt3ClientConnectionWrapper();
+            connWrapper.connection = builder.build();
+            if (connWrapper.connection == null) {
+                throw new RuntimeException("MQTT311 connection creation failed!");
+            }
+            return connWrapper;
+        } catch (Exception ex) {
+            throw new RuntimeException("Failed to create MQTT311 connection", ex);
+        }
+    }
+
+    static MqttClientConnectionWrapper createMqtt5Connection(
+            String cert, String key, String clientId, String endpoint, int port) {
+        ServiceTestLifecycleEvents lifecycleEvents = new ServiceTestLifecycleEvents();
+        try (AwsIotMqtt5ClientBuilder builder = AwsIotMqtt5ClientBuilder.newDirectMqttBuilderWithMtlsFromPath(
+                    endpoint, cert, key)) {
+            ConnectPacket.ConnectPacketBuilder connectProperties = new ConnectPacket.ConnectPacketBuilder();
+            connectProperties.withClientId(clientId);
+            builder.withConnectProperties(connectProperties);
+            builder.withLifeCycleEvents(lifecycleEvents);
+            builder.withPort((long)port);
+            Mqtt5ClientConnectionWrapper connWrapper = new Mqtt5ClientConnectionWrapper();
+            connWrapper.client = builder.build();
+            connWrapper.connection = new MqttClientConnection(connWrapper.client, null);
+            if (connWrapper.connection == null) {
+                throw new RuntimeException("MQTT5 connection creation failed!");
+            }
+            return connWrapper;
+        } catch (Exception ex) {
+            throw new RuntimeException("Failed to create MQTT311 connection from MQTT5 client", ex);
+        }
+    }
 }
