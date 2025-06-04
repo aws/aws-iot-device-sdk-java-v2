20f09d92fe310e8a95685f63673b03d5e6c3a6bd
Igor Abdrakhimov <igorabd@amazon.com>
2025-06-04 15:30:51 -0700

    Add more commands to commands-sandbox

--- samples/CommandsSandbox/src/main/java/commands/CommandsSandbox.java
+++ samples/CommandsSandbox/src/main/java/commands/CommandsSandbox.java
@@ -18,6 +18,9 @@ import software.amazon.awssdk.iot.AwsIotMqtt5ClientBuilder;
 import software.amazon.awssdk.iot.iotcommands.IotCommandsV2Client;
 import software.amazon.awssdk.iot.iotcommands.model.*;
 import software.amazon.awssdk.iot.V2ClientStreamOptions;
+import software.amazon.awssdk.services.iotjobsdataplane.IotJobsDataPlaneClient;
+import software.amazon.awssdk.services.iotjobsdataplane.model.StartCommandExecutionRequest;
+import software.amazon.awssdk.services.iotjobsdataplane.model.StartCommandExecutionResponse;
 
 import software.amazon.awssdk.iot.iotcommands.model.CommandExecutionStatus;
 import software.amazon.awssdk.regions.Region;
@@ -25,16 +28,15 @@ import software.amazon.awssdk.services.iot.IotClient;
 import software.amazon.awssdk.services.iot.model.*;
 import software.amazon.awssdk.services.sts.StsClient;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.Scanner;
 
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.CommandLine;
@@ -55,6 +57,7 @@ public class CommandsSandbox {
     static class CommandExecutionContext {
         DeviceType deviceType;
         String deviceId;
+        String deviceArn;
     }
 
     /**
@@ -73,7 +76,7 @@ public class CommandsSandbox {
         public final CompletableFuture<Void> connectedFuture = new CompletableFuture<>();
         public final CompletableFuture<Void> stoppedFuture = new CompletableFuture<>();
 
-        int nextStreamId;
+        int streamId;
         private Map<Integer, StreamingOperationContext> commandExecutionsStreams;
 
         private Map<String, CommandExecutionContext> activeCommandExecutions;
@@ -85,6 +88,7 @@ public class CommandsSandbox {
 
         public StsClient stsClient;
         public IotClient controlPlaneClient;
+        public IotJobsDataPlaneClient iotJobsDataPlaneClient;
         public Mqtt5Client protocolClient;
         public IotCommandsV2Client commandsClient;
 
@@ -95,6 +99,10 @@ public class CommandsSandbox {
 
             if (this.commandsClient != null) {
                 this.commandsClient.close();
+            }
+
+            if (this.iotJobsDataPlaneClient != null) {
+                this.iotJobsDataPlaneClient.close();
             }
 
             if (this.protocolClient != null) {
@@ -176,6 +184,15 @@ public class CommandsSandbox {
                 .region(Region.of(region))
                 .build();
 
+        DescribeEndpointRequest describeEndpointRequest =
+                DescribeEndpointRequest.builder().endpointType("iot:Jobs").build();
+        DescribeEndpointResponse describeEndpointResponse = context.controlPlaneClient.describeEndpoint(describeEndpointRequest);
+
+        context.iotJobsDataPlaneClient = IotJobsDataPlaneClient.builder()
+                .region(Region.of(region))
+                .endpointOverride(URI.create("https://" + describeEndpointResponse.endpointAddress()))
+                .build();
+
         context.thingName = commandLine.getOptionValue("thing");
         context.mqttClientId = commandLine.getOptionValue("client-id", String.format("test-%s", UUID.randomUUID()));
         context.setMqttClientArn(region);
@@ -235,11 +252,11 @@ public class CommandsSandbox {
 
         MqttRequestResponseClientOptions rrClientOptions = MqttRequestResponseClientOptions.builder()
                 .withMaxRequestResponseSubscriptions(5)
-                .withMaxStreamingSubscriptions(2)
+                .withMaxStreamingSubscriptions(10)
                 .withOperationTimeoutSeconds(30)
                 .build();
 
-        context.nextStreamId = 1;
+        context.streamId = 0;
         context.commandExecutionsStreams = new HashMap<Integer, StreamingOperationContext>();
 
         context.activeCommandExecutions = new ConcurrentHashMap<String, CommandExecutionContext>();
@@ -276,21 +293,29 @@ public class CommandsSandbox {
         System.out.println("                     <content-type> a content type of the payload");
         System.out.println("                         JSON and CBOR are handled specifically, see README for more information");
         System.out.println("    delete-command <command-id>");
-        System.out.println("                     deletes a command with the specified command ID\n");
+        System.out.println("                     deletes a command with the specified command ID");
+        System.out.println("    send-command-to-thing <command-id> [<timeout>]");
+        System.out.println("                     creates an AWS IoT Command execution targeted for the IoT thing specified");
+        System.out.println("                     at the application start");
+        System.out.println("    send-command-to-client <command-id> [<timeout>]");
+        System.out.println("                     creates an AWS IoT Command execution targeted for the MQTT client specified");
+        System.out.println("                     at the application start");
+        System.out.println("    get-command-execution <command-execution-id>");
+        System.out.println("                     Get status of the specified AWS IoT command execution\n");
         System.out.println("  MQTT Command service commands:");
         System.out.println("    open-thing-stream <payload-format>");
         System.out.println("                     subscribe to a stream of command executions with a specified payload format");
         System.out.println("                     targeting the IoT Thing set on the application startup");
         System.out.println("                     <payload-format> is a string, with the following special values:");
-        System.out.println("                         json - subscribe to commands with JSON payload");
-        System.out.println("                         cbor - subscribe to commands with CBOR payload");
+        System.out.println("                         application/json - subscribe to commands with JSON payload");
+        System.out.println("                         application/cbor - subscribe to commands with CBOR payload");
         System.out.println("                         for any other value, subscribe to a generic topic");
         System.out.println("    open-client-stream <payload-format>");
         System.out.println("                     subscribe to a stream of command executions with a specified payload format");
         System.out.println("                     targeting the MQTT client ID set on the application startup");
         System.out.println("                     <payload-format> is a string, with the following special values:");
-        System.out.println("                         json - subscribe to commands with JSON payload");
-        System.out.println("                         cbor - subscribe to commands with CBOR payload");
+        System.out.println("                         application/json - subscribe to commands with JSON payload");
+        System.out.println("                         application/cbor - subscribe to commands with CBOR payload");
         System.out.println("                         for any other value, subscribe to a generic topic");
         System.out.println("    update-command-execution <executionId> <status> [<reason-code>] [<reason-description>]");
         System.out.println("                     updates a command execution with a new status");
@@ -342,6 +367,57 @@ public class CommandsSandbox {
         }
     }
 
+    private static void handleSendCommand(ApplicationContext context, DeviceType deviceType, String arguments) {
+        String[] argumentSplit = arguments.trim().split(" ", 2);
+
+        String commandId = argumentSplit[0];
+        Long timeout = 10L;
+        if (argumentSplit.length > 1) {
+            timeout = Long.parseLong(argumentSplit[1]);
+        }
+
+        String deviceArn;
+        if (deviceType == DeviceType.THING) {
+            deviceArn = context.thingArn;
+        } else {
+            deviceArn = context.mqttClientArn;
+        }
+
+        try {
+            GetCommandRequest getCommandRequest = GetCommandRequest.builder().commandId(commandId).build();
+            GetCommandResponse getCommandResponse = context.controlPlaneClient.getCommand(getCommandRequest);
+
+            StartCommandExecutionRequest request = StartCommandExecutionRequest.builder()
+                    .commandArn(getCommandResponse.commandArn())
+                    .executionTimeoutSeconds(timeout)
+                    .targetArn(deviceArn)
+                    .build();
+
+            context.iotJobsDataPlaneClient.startCommandExecution(request);
+        } catch (Exception ex) {
+            handleOperationException("send-command", ex, context);
+        }
+    }
+
+    private static void handleGetCommandExecution(ApplicationContext context, String arguments) {
+        String commandExecutionId = arguments.trim();
+
+        try {
+            if (context.activeCommandExecutions.containsKey(commandExecutionId)) {
+                CommandExecutionContext commandExecutionContext = context.activeCommandExecutions.get(commandExecutionId);
+
+                GetCommandExecutionRequest getCommandExecutionRequest = GetCommandExecutionRequest.builder()
+                        .executionId(commandExecutionId)
+                        .targetArn(commandExecutionContext.deviceArn)
+                        .build();
+                GetCommandExecutionResponse getCommandExecutionResponse = context.controlPlaneClient.getCommandExecution(getCommandExecutionRequest);
+                System.out.printf("Status of command execution '%s' is %s\n", commandExecutionId, getCommandExecutionResponse.status());
+            }
+        } catch (Exception ex) {
+            handleOperationException("get-command-execution", ex, context);
+        }
+    }
+
     private static void handleUpdateCommandExecution(ApplicationContext context, String arguments) {
         String[] argumentSplit = arguments.trim().split(" ", 2);
         if (argumentSplit.length < 2) {
@@ -370,24 +446,47 @@ public class CommandsSandbox {
     private static void handleOpenStream(ApplicationContext context, DeviceType deviceType, String payloadFormat) {
         try {
             String deviceId;
+            String deviceArn;
             if (deviceType == DeviceType.THING) {
                 deviceId = context.thingName;
+                deviceArn = context.thingArn;
             } else {
                 deviceId = context.mqttClientId;
+                deviceArn = context.mqttClientArn;
             }
+
+            int streamId = context.streamId++;
+            StreamingOperationContext streamingOperationContext = new StreamingOperationContext();
+            context.commandExecutionsStreams.put(streamId, streamingOperationContext);
 
             V2ClientStreamOptions<CommandExecutionEvent> options =
                     V2ClientStreamOptions.<CommandExecutionEvent>builder().withStreamEventHandler(event -> {
-                        System.out.printf("Received command execution event ID '%s' with payload type '%s'\n", event.executionId, event.contentType);
+                        System.out.println("Received new command execution");
+                        System.out.printf("  execution ID: %s\n", event.executionId);
+                        System.out.printf("  payload format: %s\n", event.contentType);
+                        System.out.printf("  execution timeout: %d\n", event.timeout);
+                        System.out.printf("  payload size: %d\n", event.payload.length);
+                        if (event.contentType.equals("application/json")) {
+                            String payload = new String(event.payload, StandardCharsets.UTF_8);
+                            System.out.printf("  JSON payload: '%s'\n", payload);
+                        }
                         CommandExecutionContext commandExecutionContext = new CommandExecutionContext();
                         commandExecutionContext.deviceId = deviceId;
                         commandExecutionContext.deviceType = deviceType;
+                        commandExecutionContext.deviceArn = deviceArn;
                         context.activeCommandExecutions.put(event.executionId, commandExecutionContext);
-                    }).build();
+                    }).withSubscriptionEventHandler(
+                            event -> {
+                                if (event.getError().isPresent()) {
+                                    System.out.printf("Error on opening stream: %d (%s)", event.getError().get(), CRT.awsErrorString(event.getError().get()));
+                                    context.commandExecutionsStreams.remove(streamId);
+                                    streamingOperationContext.operation.close();
+                                }
+                            }
+                    ).build();
 
             CommandExecutionsSubscriptionRequest request = new CommandExecutionsSubscriptionRequest();
 
-            StreamingOperationContext streamingOperationContext = new StreamingOperationContext();
             streamingOperationContext.deviceType = deviceType;
             streamingOperationContext.deviceId = deviceId;
             streamingOperationContext.payloadType = payloadFormat;
@@ -396,10 +495,10 @@ public class CommandsSandbox {
             request.deviceId = deviceId;
 
             switch (payloadFormat) {
-                case "json":
+                case "application/json":
                     streamingOperationContext.operation = context.commandsClient.createCommandExecutionsJsonPayloadStream(request, options);
                     break;
-                case "cbor":
+                case "application/cbor":
                     streamingOperationContext.operation = context.commandsClient.createCommandExecutionsCborPayloadStream(request, options);
                     break;
                 default:
@@ -408,10 +507,8 @@ public class CommandsSandbox {
             }
 
             streamingOperationContext.operation.open();
-            System.out.printf("Opened streaming operation with ID %d\n", context.nextStreamId);
+            System.out.printf("Opened streaming operation with ID %d\n", context.streamId);
 
-            context.commandExecutionsStreams.put(context.nextStreamId, streamingOperationContext);
-            context.nextStreamId++;
 
         } catch (Exception ex) {
             handleOperationException("open-command-stream", ex, context);
@@ -455,6 +552,21 @@ public class CommandsSandbox {
             case "delete-command":
                 if (commandLineSplit.length == 2) {
                     handleDeleteCommand(context, commandLineSplit[1]);
+                }
+                return false;
+            case "send-command-to-thing":
+                if (commandLineSplit.length == 2) {
+                    handleSendCommand(context, DeviceType.THING, commandLineSplit[1]);
+                }
+                return false;
+            case "send-command-to-client":
+                if (commandLineSplit.length == 2) {
+                    handleSendCommand(context, DeviceType.CLIENT, commandLineSplit[1]);
+                }
+                return false;
+            case "get-command-execution":
+                if (commandLineSplit.length == 2) {
+                    handleGetCommandExecution(context, commandLineSplit[1]);
                 }
                 return false;
             case "open-thing-stream":
@@ -505,12 +617,11 @@ public class CommandsSandbox {
             boolean done = false;
             Scanner scanner = new Scanner(System.in);
             while (!done) {
-                System.out.print("Enter command: > ");
+                System.out.print("\nEnter command: > ");
                 if (scanner.hasNextLine()) {
                     String userInput = scanner.nextLine();
                     done = handleCommand(userInput, context);
-                }
-                else {
+                } else {
                     done = true;
                 }
             }
