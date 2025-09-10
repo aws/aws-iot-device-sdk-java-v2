6b2035e84584a5e27c496e2702336e6d8a550e19
Steve Kim <sbstevek@amazon.com>
2025-09-10 13:54:24 -0700

    java x509 sample update

--- samples/Mqtt/X509PubSub/src/main/java/x509pubsub/X509PubSub.java
+++ samples/Mqtt/X509PubSub/src/main/java/x509pubsub/X509PubSub.java
@@ -5,19 +5,6 @@
 
 package mqtt.x509pubsub;
 
-import software.amazon.awssdk.crt.CRT;
-import software.amazon.awssdk.crt.CrtResource;
-import software.amazon.awssdk.crt.CrtRuntimeException;
-import software.amazon.awssdk.crt.io.ClientBootstrap;
-import software.amazon.awssdk.crt.mqtt5.*;
-import software.amazon.awssdk.crt.mqtt5.Mqtt5ClientOptions.LifecycleEvents;
-import software.amazon.awssdk.crt.mqtt5.packets.*;
-import software.amazon.awssdk.iot.AwsIotMqtt5ClientBuilder;
-
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
-// CHECK THESE
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Arrays;
@@ -25,19 +12,14 @@ import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import software.amazon.awssdk.crt.CrtResource;
+import software.amazon.awssdk.crt.CrtRuntimeException;
+import software.amazon.awssdk.crt.mqtt5.*;
+import software.amazon.awssdk.crt.mqtt5.packets.*;
+import software.amazon.awssdk.iot.AwsIotMqtt5ClientBuilder;
+
 /**
  * MQTT5 X509 Sample (mTLS)
- *
- * Usage (macOS/Linux):
- *   java -cp target/classes:target/deps/* samples.Mqtt5X509Sample \
- *     --endpoint <endpoint> \
- *     --cert </path/certificate.pem> \
- *     --key </path/private.key> \
- *     [--ca_file </path/AmazonRootCA1.pem>] \
- *     [--client-id my-client] \
- *     [--topic test/topic] \
- *     [--message "Hello from mqtt5 sample"] \
- *     [--count 5]
  */
 public class X509PubSub {
 
@@ -110,11 +92,16 @@ public class X509PubSub {
         System.out.println("\nStarting MQTT5 X509 PubSub Sample\n");
         final int TIMEOUT_SECONDS = 100;
 
-        // Latches for flow control of Sample
+        /*
+         * Latches for flow control of Sample
+         */
         CountDownLatch connected = new CountDownLatch(1);
         CountDownLatch stopped = new CountDownLatch(1);
-        CountDownLatch receivedAll = new CountDownLatch(args.count > 0 ? args.count : 1); // if infinite, we won't await fully
+        CountDownLatch receivedAll = new CountDownLatch(args.count > 0 ? args.count : 1);
 
+        /*
+         * Handle MQTT5 Client lifecycle events
+         */
         Mqtt5ClientOptions.LifecycleEvents lifecycleEvents = new Mqtt5ClientOptions.LifecycleEvents() {
             @Override
             public void onAttemptingConnect(Mqtt5Client client, OnAttemptingConnectReturn onAttemptingConnectReturn) {
@@ -152,6 +139,9 @@ public class X509PubSub {
             }
         };
 
+        /*
+         * Handle Publishes received by the MQTT5 Client
+         */
         Mqtt5ClientOptions.PublishEvents publishEvents = new Mqtt5ClientOptions.PublishEvents() {
             @Override
             public void onMessageReceived(Mqtt5Client client, PublishReturn publishReturn) {
@@ -167,102 +157,105 @@ public class X509PubSub {
             }
         };
 
+        Mqtt5Client client;
+
+        /**
+         * Create MQTT5 client using mutual TLS via X509 Certificate and Private Key
+         */
+        System.out.println("==== Creating MQTT5 Client ====\n");
+        AwsIotMqtt5ClientBuilder builder = AwsIotMqtt5ClientBuilder.newDirectMqttBuilderWithMtlsFromPath(
+            args.endpoint,args.certPath, args.keyPath);
+        ConnectPacket.ConnectPacketBuilder connectProperties = new ConnectPacket.ConnectPacketBuilder();
+        connectProperties.withClientId(args.clientId);
+        builder.withConnectProperties(connectProperties);
+        builder.withLifeCycleEvents(lifecycleEvents);
+        builder.withPublishEvents(publishEvents);
+        client = builder.build();
+        builder.close();
+
+        System.out.println("==== Starting client ====");
+        client.start();
         try {
-            Mqtt5Client client;
-
-            /**
-             * Create MQTT5 client using mutual TLS via X509 Certificate and Private Key
-             */
-            System.out.println("==== Creating MQTT5 Client ====\n");
-            AwsIotMqtt5ClientBuilder builder = AwsIotMqtt5ClientBuilder.newDirectMqttBuilderWithMtlsFromPath(
-                args.endpoint,args.certPath, args.keyPath);
-            ConnectPacket.ConnectPacketBuilder connectProperties = new ConnectPacket.ConnectPacketBuilder();
-            connectProperties.withClientId(args.clientId);
-            builder.withConnectProperties(connectProperties);
-            builder.withLifeCycleEvents(lifecycleEvents);
-            builder.withPublishEvents(publishEvents);
-            client = builder.build();
-            builder.close();
-
-
-            System.out.println("==== Starting client ====");
-            client.start();
             if (!connected.await(TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
                 throw new RuntimeException("Connection timeout");
             }
+        } catch (InterruptedException ex) {
+            onApplicationFailure(ex);
+        }
 
+        /* Subscribe */
+        System.out.printf("==== Subscribing to topic '%s' ====%n", args.topic);
+        SubscribePacket subscribePacket = SubscribePacket.of(args.topic, QOS.AT_LEAST_ONCE);
+        try {
+            SubAckPacket subAckPacket  = client.subscribe(subscribePacket).get(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+            System.out.println("SubAck received with reason code:" + subAckPacket.getReasonCodes() + "\n");
+        } catch (Exception ex) {
+            onApplicationFailure(ex);
+        }
 
-            /* Subscribe */
-            System.out.printf("==== Subscribing to topic '%s' ====%n", args.topic);
-            SubscribePacket subscribePacket = SubscribePacket.of(args.topic, QOS.AT_LEAST_ONCE);
+        /* Publish */
+        if (args.count == 0) {
+            System.out.println("==== Sending messages until program killed ====\n");
+        } else {
+            System.out.printf("==== Sending %d message(s) ====%n%n", args.count);
+        }
+        int publishCount = 1;
+        while (args.count == 0 || publishCount <= args.count) {
+            String payload = args.message + " [" + publishCount + "]";
+            System.out.printf("Publishing message to topic '%s': %s%n", args.topic, payload);
+            PublishPacket publishPacket = PublishPacket.of(
+                args.topic,
+                QOS.AT_LEAST_ONCE,
+                payload.getBytes(StandardCharsets.UTF_8));
             try {
-                SubAckPacket subAckPacket  = client.subscribe(subscribePacket).get(TIMEOUT_SECONDS, TimeUnit.SECONDS);
-                System.out.println("SubAck received with reason code:" + subAckPacket.getReasonCodes() + "\n");
+            PubAckPacket pubAck = client.publish(publishPacket).get(TIMEOUT_SECONDS, TimeUnit.SECONDS).getResultPubAck();
+            System.out.println("PubAck received with reason: " + pubAck.getReasonCode() + "\n");
             } catch (Exception ex) {
                 onApplicationFailure(ex);
             }
-
-            /* Publish */
-            if (args.count == 0) {
-                System.out.println("==== Sending messages until program killed ====\n");
-            } else {
-                System.out.printf("==== Sending %d message(s) ====%n%n", args.count);
+            try {
+                Thread.sleep(Duration.ofMillis(1500).toMillis());
+            } catch (InterruptedException ex) {
+                onApplicationFailure(ex);
             }
-
-            int publishCount = 1;
-            while (args.count == 0 || publishCount <= args.count) {
-                String payload = args.message + " [" + publishCount + "]";
-                System.out.printf("Publishing message to topic '%s': %s%n", args.topic, payload);
-
-                PublishPacket publishPacket = PublishPacket.of(
-                    args.topic,
-                    QOS.AT_LEAST_ONCE,
-                    payload.getBytes(StandardCharsets.UTF_8));
-
+            publishCount++;
+        }
+        if (args.count > 0) {
+            long remaining = receivedAll.getCount();
+            if (remaining > 0) {
                 try {
-                PubAckPacket pubAck = client.publish(publishPacket).get(TIMEOUT_SECONDS, TimeUnit.SECONDS).getResultPubAck();
-                System.out.println("PubAck received with reason: " + pubAck.getReasonCode() + "\n");
-                } catch (Exception ex) {
+                    receivedAll.await(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+                } catch (InterruptedException ex) {
                     onApplicationFailure(ex);
                 }
-
-                Thread.sleep(Duration.ofMillis(1500).toMillis());
-                publishCount++;
             }
+            long received = (args.count - receivedAll.getCount());
+            System.out.printf("%d message(s) received.%n%n", received);
+        }
 
-            if (args.count > 0) {
-                long remaining = receivedAll.getCount();
-                if (remaining > 0) {
-                    receivedAll.await(TIMEOUT_SECONDS, TimeUnit.SECONDS);
-                }
-                long received = (args.count - receivedAll.getCount());
-                System.out.printf("%d message(s) received.%n%n", received);
-            }
+        // ---------- Unsubscribe ----------
+        System.out.printf("==== Unsubscribing from topic '%s' ====%n", args.topic);
+        UnsubscribePacket unsubscribePacket = UnsubscribePacket.of(args.topic);
+        try {
+            UnsubAckPacket unsubAckPacket = client.unsubscribe(unsubscribePacket).get(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+            System.out.println("UnsubAck received with reason code:" + unsubAckPacket.getReasonCodes() + "\n");
+        } catch (Exception ex) {
+            onApplicationFailure(ex);
+        }
 
-            // ---------- Unsubscribe ----------
-            System.out.printf("==== Unsubscribing from topic '%s' ====%n", args.topic);
-            UnsubscribePacket unsubscribePacket = UnsubscribePacket.of(args.topic);
-            try {
-                UnsubAckPacket unsubAckPacket = client.unsubscribe(unsubscribePacket).get(TIMEOUT_SECONDS, TimeUnit.SECONDS);
-                System.out.println("UnsubAck received with reason code:" + unsubAckPacket.getReasonCodes() + "\n");
-            } catch (Exception ex) {
-                onApplicationFailure(ex);
-            }
-
-            
-            System.out.println("==== Stopping Client ====");
-            client.stop();
+        System.out.println("==== Stopping Client ====");
+        client.stop();
+        try {
             if (!stopped.await(TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
                 throw new RuntimeException("Stop timeout");
             }
-            System.out.println("==== Client Stopped! ====");
-
-            /* Close the client to free memory */
-            client.close();
-
-        } catch (CrtRuntimeException | InterruptedException ex) {
+        } catch (InterruptedException ex) {
             onApplicationFailure(ex);
         }
+        System.out.println("==== Client Stopped! ====");
+
+        /* Close the client to free memory */
+        client.close();        
 
         CrtResource.waitForNoResources();
         System.out.println("Complete!");
