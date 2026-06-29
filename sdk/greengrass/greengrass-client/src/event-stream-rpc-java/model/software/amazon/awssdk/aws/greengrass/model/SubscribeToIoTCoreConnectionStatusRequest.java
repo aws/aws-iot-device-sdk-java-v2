836e2dc37a5959a2e0814b738bfa4c61e9e48efa
Vera Xia <zhvxia@amazon.com>
2026-06-29 16:32:52 -0700

    Greengrass update (#695)
    * code gen update
    
    * builder update
    
    * update builder version

---
+++ sdk/greengrass/greengrass-client/src/event-stream-rpc-java/model/software/amazon/awssdk/aws/greengrass/model/SubscribeToIoTCoreConnectionStatusRequest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ *
+ * This file is generated.
+ */
+
+package software.amazon.awssdk.aws.greengrass.model;
+
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.util.Objects;
+import software.amazon.awssdk.eventstreamrpc.model.EventStreamJsonMessage;
+
+public class SubscribeToIoTCoreConnectionStatusRequest implements EventStreamJsonMessage {
+  public static final String APPLICATION_MODEL_TYPE = "aws.greengrass#SubscribeToIoTCoreConnectionStatusRequest";
+
+  public static final SubscribeToIoTCoreConnectionStatusRequest VOID;
+
+  static {
+    VOID = new SubscribeToIoTCoreConnectionStatusRequest() {
+      @Override
+      public boolean isVoid() {
+        return true;
+      }
+    };
+  }
+
+  public SubscribeToIoTCoreConnectionStatusRequest() {
+  }
+
+  @Override
+  public String getApplicationModelType() {
+    return APPLICATION_MODEL_TYPE;
+  }
+
+  @Override
+  public boolean equals(Object rhs) {
+    if (rhs == null) return false;
+    if (!(rhs instanceof SubscribeToIoTCoreConnectionStatusRequest)) return false;
+    if (this == rhs) return true;
+    final SubscribeToIoTCoreConnectionStatusRequest other = (SubscribeToIoTCoreConnectionStatusRequest)rhs;
+    boolean isEquals = true;
+    return isEquals;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash();
+  }
+}
